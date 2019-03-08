868ec306c0ae39ffe80eaa0473a05ee0631d6321
qtrinh128 <quangtrinh128@gmail.com>
2019-03-08 18:39:27 +0700

    first commit

---
+++ ServiceCheckOrderTool/Model/ModelService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceCheckOrderTool.Model
+{
+    class ModelService
+    {
+        public static void WriteLogError(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString("g") + ": " + ex.Source + "; " + ex.Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        public static void WriteLogError(string message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString("g") + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        public static void OpenTool(string path)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + $"{path}" + ".exe";
+            process.Start();
+        }
+    }
+}
---
+++ ServiceCheckOrderTool/Service1.cs
@@ -0,0 +1,48 @@
+﻿using ServiceCheckOrderTool.Model;
+using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ServiceCheckOrderTool
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        private Timer timer = null;
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer
+            {
+                Interval = 3600000
+            };
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            decimal hour = time.Hour;
+            int check = Convert.ToInt32(hour);
+            if (check == 0)
+            {
+                string path = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "config\\path.txt");
+                ModelService.OpenTool(path);
+            }
+            //string path = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "config\\path.txt");
+            //ModelService.OpenTool(path);
+            //ModelService.WriteLogError(hour.ToString());
+        }
+
+        protected override void OnStop()
+        {
+            ModelService.WriteLogError("WindowsService has been stop");
+        }
+    }
+}
